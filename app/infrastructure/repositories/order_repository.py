a803fa7d50b9fa7562ac1220cc177c7a282f7a14
Sebastian Dehlsen <sonnic890@live.dk>
2026-05-15 11:54:18 +0100

    implement sqlalchemy order repository

--- app/infrastructure/repositories/order_repository.py
+++ app/infrastructure/repositories/order_repository.py
@@ -3,6 +3,15 @@ from abc import ABC, abstractmethod
 from app.domain.orders.entities import Order
 from app.domain.orders.value_objects import OrderId
 
+from sqlalchemy.orm import Session
+
+from app.infrastructure.db.models import (
+    OrderModel,
+)
+from app.infrastructure.mappers.order_mapper import (
+    OrderMapper,
+)
+
 
 class OrderRepository(ABC):
 
@@ -36,3 +45,44 @@ class InMemoryOrderRepository(OrderRepository):
         order_id: OrderId,
     ) -> Order | None:
         return self._orders.get(order_id)
+    
+class SqlAlchemyOrderRepository(
+    OrderRepository
+):
+
+    def __init__(
+        self,
+        session: Session,
+    ) -> None:
+
+        self._session = session
+
+    def save(
+        self,
+        order: Order,
+    ) -> None:
+
+        model = OrderMapper.to_model(
+            order
+        )
+
+        self._session.merge(
+            model
+        )
+
+    def get_by_id(
+        self,
+        order_id: OrderId,
+    ) -> Order | None:
+
+        model = self._session.get(
+            OrderModel,
+            str(order_id.value),
+        )
+
+        if model is None:
+            return None
+
+        return OrderMapper.to_domain(
+            model
+        )
